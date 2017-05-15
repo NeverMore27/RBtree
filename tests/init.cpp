93ab2df057ad5fe56bf601de2f1d7e9a46396e20
NeverMore27 <uragan.uragan.59@mail.ru>
2017-05-15 19:35:01 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -30,23 +30,27 @@ SCENARIO("BST delete root without child", "[delete]")
 {
 	RBtree<int> tree = { 8, 4, 3 };
 	tree.Remove(8);
-	REQUIRE(tree == RBtree<int>({ 4, 3 }));
+	RBtree<int> tr ={ 4, 3 };
+	REQUIRE(tree ==tr);
 }
 SCENARIO("BST delete root with two children", "[delete]")
 {
 	RBtree<int> tree = { 8, 4, 3, 10, 9, 13, 11, 12 };
 	tree.Remove(4);
-	REQUIRE(tree == RBtree<int>({ 9, 8,  11, 3, 10, 13, 12 });
+	RBtree<int>tr=={ 9, 8,  11, 3, 10, 13, 12 };
+	REQUIRE(tree ==tr );
 }
 SCENARIO("BST delete non root without children", "[delete]")
 {
 	RBtree<int> tree = { 8, 4, 3, 10, 9, 13, 11, 12 };
 	tree.Remove(3);
-	REQUIRE(tree == RBtree<int>({ 9,  4, 11, 10, 12, 13, 8 });
+	RBtree<int>tr= { 9,  4, 11, 10, 12, 13, 8 };
+	REQUIRE(tree == tr);
 }
 SCENARIO("BST delete non root with one child", "[delete]")
 {
 	RBtree<int> tree = { 8, 4, 3, 10, 9, 13, 11, 12 };
 	tree.Remove(12);
-	REQUIRE(tree == RBtree<int>({ 9,  4, 11, 10, 4, 13, 8 });
+	RBtree<int> tr ={ 9,  4, 11, 10, 4, 13, 8 }
+	REQUIRE(tree == tr);
 }
