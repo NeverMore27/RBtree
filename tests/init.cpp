170bc7f284ffc7d6484afd28b50dc7ae17bb3a7a
NeverMore27 <uragan.uragan.59@mail.ru>
2017-05-15 19:26:38 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -8,45 +8,45 @@ SCENARIO("null")
 	RBtree<int> a;
 	REQUIRE(a.isEmpty());
 }
-SCENARIO("add") 
+SCENARIO("add")
 {
 	RBtree <int> a;
 	a.Insert(5);
 	REQUIRE(!a.isEmpty());
 }
 SCENARIO("BST delete non inserted element", "[delete]")
-{ 
-	RBtree<int> tree = {8}; 
+{
+	RBtree<int> tree = { 8 };
 	tree.Remove(4);
-	REQUIRE(!tree.isEmpty()); 
-} 
-SCENARIO("BST delete root with one element", "[delete]") 
-{ 
-	RBtree<int> tree = {8}; 
-	 tree.Remove(8); 
-	REQUIRE( tree.isEmpty() ); 
-} 
-SCENARIO("BST delete root without child", "[delete]") 
-{ 
-	RBtree<int> tree = {8, 4, 3}; 
+	REQUIRE(!tree.isEmpty());
+}
+SCENARIO("BST delete root with one element", "[delete]")
+{
+	RBtree<int> tree = { 8 };
 	tree.Remove(8);
-	REQUIRE( tree ==RBtree<int>({4, 3}) ); 
-} 
-SCENARIO("BST delete root with two children", "[delete]") 
-{ 
-	RBtree<int> tree = {8, 4, 3, 10, 9, 13, 11, 12}; 
-	tree.Remove(4); 
-	REQUIRE( tree == RBtree<int>({ 9, 8,  11, 3, 10, 13, 12 });
+	REQUIRE(tree.isEmpty());
 }
-SCENARIO("BST delete non root without children", "[delete]") 
-{ 
-	RBtree<int> tree = {8, 4, 3, 10, 9, 13, 11, 12}; 
-	tree.Remove(3); 
-	REQUIRE( tree == RBtree<int>({ 9,  4, 11, 10, 12, 13, 8 }) );
+SCENARIO("BST delete root without child", "[delete]")
+{
+	RBtree<int> tree = { 8, 4, 3 };
+	tree.Remove(8);
+	REQUIRE(tree == RBtree<int>({ 4, 3 }));
 }
-SCENARIO("BST delete non root with one child", "[delete]") 
-{ 
-	RBtree<int> tree = {8, 4, 3, 10, 9, 13, 11, 12}; 
-	tree.Remove(12); 
-	REQUIRE( tree == RBtree<int>({ 9,  4, 11, 10, 4, 13, 8 }) );
+SCENARIO("BST delete root with two children", "[delete]")
+{
+	RBtree<int> tree = { 8, 4, 3, 10, 9, 13, 11, 12 };
+	tree.Remove(4);
+	REQUIRE(tree == RBtree<int>({ 9, 8,  11, 3, 10, 13, 12 });
+}
+SCENARIO("BST delete non root without children", "[delete]")
+{
+	RBtree<int> tree = { 8, 4, 3, 10, 9, 13, 11, 12 };
+	tree.Remove(3);
+	REQUIRE(tree == RBtree<int>({ 9,  4, 11, 10, 12, 13, 8 });
+}
+SCENARIO("BST delete non root with one child", "[delete]")
+{
+	RBtree<int> tree = { 8, 4, 3, 10, 9, 13, 11, 12 };
+	tree.Remove(12);
+	REQUIRE(tree == RBtree<int>({ 9,  4, 11, 10, 4, 13, 8 });
 }
