ecbc4486b5bac418bccd47ac5f5c8988bdc09151
NeverMore27 <uragan.uragan.59@mail.ru>
2017-12-15 23:02:26 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -5,52 +5,53 @@
 
 SCENARIO("null")
 {
-	RBtree<int> a;
+	RBtree<int, int> a;
 	REQUIRE(a.isEmpty());
 }
 SCENARIO("add")
 {
-	RBtree <int> a;
+	RBtree <int, int> a;
 	a.Insert(5);
 	REQUIRE(!a.isEmpty());
 }
 SCENARIO("BST delete non inserted element", "[delete]")
 {
-	RBtree<int> tree = { 8 };
+	RBtree<int, int> tree = { {8, 7} };
 	tree.Remove(4);
 	REQUIRE(!tree.isEmpty());
 }
 SCENARIO("BST delete root with one element", "[delete]")
 {
-	RBtree<int> tree = { 8 };
+	RBtree<int, int> tree = { {8,8} };
 	tree.Remove(8);
 	REQUIRE(tree.isEmpty());
 }
 SCENARIO("BST delete root without child", "[delete]")
 {
-	RBtree<int> tree = { 8, 4, 3 };
+	RBtree<int> tree = { {2,8}, {5, 4}, {5, 3} };
 	tree.Remove(8);
-	RBtree<int> tr ={ 4, 3 };
+	RBtree<int> tr ={ {5, 4}, {5, 3}};
 	REQUIRE(tree ==tr);
 }
 SCENARIO("BST delete root with two children", "[delete]")
 {
-	RBtree<int> tree = { 8, 4, 3, 10, 9, 13, 11, 12 };
+	RBtree<int, int> tree = { {1, 8}, {18, 4}, {22, 3}, {2,10}, {3, 9}, {12,13}, {7,11}, {1,12} };
 	tree.Remove(4);
-	RBtree<int>tr={ 9, 8,  11, 3, 10, 13, 12 };
+	RBtree<int>tr={ {3, 9}, {1, 8},  {7,11}, {22, 3},{2,10}, {12,13}, {1,12} };
 	REQUIRE(tree ==tr );
 }
 SCENARIO("BST delete non root without children", "[delete]")
 {
-	RBtree<int> tree = { 8, 4, 3, 10, 9, 13, 11, 12 };
+	RBtree<int> tree = { {1, 8}, {18, 4}, {22, 3}, {2,10}, {3, 9}, {12,13}, {7,11}, {1,12} };
 	tree.Remove(3);
-	RBtree<int>tr= { 9,  4, 11, 10, 12, 13, 8 };
+	RBtree<int>tr={{3, 9}, {18, 4}, {7,11}, {2,10}, {1,12}, {12,13}, {1, 8}} 
 	REQUIRE(tree == tr);
 }
 SCENARIO("BST delete non root with one child", "[delete]")
 {
-	RBtree<int> tree = { 8, 4, 3, 10, 9, 13, 11, 12 };
+	RBtree<int> tree = { {1, 8}, {18, 4}, {22, 3}, {2,10}, {3, 9}, {12,13}, {7,11}, {1,12} };
 	tree.Remove(12);
-	RBtree<int> tr ={ 9,  4, 11, 10, 4, 13, 8, 3};
+	RBtree<int> tr ={{3, 9}, {18, 4}, {7,11}, {2,10}, {18, 4}, {12,13}, {1, 8}, {22, 3}}
+	{ 9,  4, 11, 10, 4, 13, 8, 3};
 	REQUIRE(tree == tr);
 }
